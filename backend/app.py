f61c7d87e2741b42d9f2aad5ac954d78f3ff667d
rahul <rahul2007.rsrv@gmail.com>
2026-08-25 15:30:04 +0530

    Temporarily allowing all for testing

--- backend/app.py
+++ backend/app.py
@@ -9,8 +9,8 @@ app = FastAPI()
 # Enable CORS for React frontend
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000","https://newslens-2qn3.onrender.com"],
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
